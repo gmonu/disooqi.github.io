00ed1ec4a29190b79a65477214ff9422cb463b9c
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-09-29 14:08:34 +0300

    manipulate blog imgs to be cropped

--- img_manipulation.py
+++ img_manipulation.py
@@ -3,8 +3,21 @@
 from PIL import Image
 
 
-size_750x300 = (750, 200)
+size_750x300 = 750,300
 #
 img = Image.open('blog/img/maxresdefault.jpg')
+
+
+
 img.thumbnail(size_750x300)
-img.save('blog/img/750x300/venv.png')
+width = img.size[0]
+height = img.size[1]
+img_cropped = img.crop(
+    (
+        0,
+        50,
+        width,
+        height-50
+    )
+)
+img_cropped.save('blog/img/750x300/venv.png')
